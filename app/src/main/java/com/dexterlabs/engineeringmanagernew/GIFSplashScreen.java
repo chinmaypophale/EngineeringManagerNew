4d817040f061135029fb63db6a784ceccfbe33ac
chinmaypophale <37659306+chinmaypophale@users.noreply.github.com>
2018-06-17 02:17:30 +0530

    Initial commit

---
+++ app/src/main/java/com/dexterlabs/engineeringmanagernew/GIFSplashScreen.java
@@ -0,0 +1,46 @@
+package com.dexterlabs.engineeringmanagernew;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class GIFSplashScreen extends AppCompatActivity {
+
+    private GifImageView gifImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gifsplash_screen);
+
+        gifImageView =(GifImageView)findViewById(R.id.gifImageView);
+
+        try {
+            InputStream inputStream=getAssets().open("beargif.gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+           GIFSplashScreen.this.startActivity(new Intent(GIFSplashScreen.this ,WelcomeActivity.class));
+           GIFSplashScreen.this.finish();
+            }
+        },5000);
+
+    }
+}
